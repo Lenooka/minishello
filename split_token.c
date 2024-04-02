738a139fe6716df33022a855307b90788239ff95
Olena <lenanavorska@gmail.com>
2024-04-02 19:16:27 +0200

    spliit

--- split_token.c
+++ split_token.c
@@ -6,51 +6,19 @@
 /*   By: otolmach <otolmach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 18:53:11 by otolmach          #+#    #+#             */
-/*   Updated: 2024/04/02 18:53:12 by otolmach         ###   ########.fr       */
+/*   Updated: 2024/04/02 19:04:02 by otolmach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
-char	*ft_strncpy(char *dest, char *s, int n)
+int	count_words(char *str)
 {
 	int	i;
 
 	i = 0;
-	while ((s[i] != '\0') && (i < n))
-	{
-		dest[i] = s[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	
 }
-
-char	*ft_strndup(t_mnshll *mnshll, char *s, int n)
-{
-	char	*cs;
-
-	cs = (char *)malloc(sizeof(char) * (n + 1));
-	if (!cs)
-	{
-		return (NULL);
-	}
-	cs = ft_strncpy(cs, s, n);
-	return (cs);
-}
-void	free_arrays(char **str_tab, int i)
-{
-	int	j;
-
-	j = 0;
-	while (j < i && str_tab[j] != 0)
-	{
-		free(str_tab[j]);
-		j++;
-	}
-	free(str_tab);
-	return (NULL);
-}
-
 
 char	**split_tokenize(t_mnshll *minsh, char *str)
 {
@@ -60,7 +28,7 @@ char	**split_tokenize(t_mnshll *minsh, char *str)
 	int		toklen;
 
 	i = 0;
-	token_ammount = count_words(minsh) + 1;
+	token_ammount = count_words(str) + 1;
 	result = (char **)malloc(sizeof(char *) * (token_ammount));
 	if (!result)
 		return (NULL);
---
+++ split_token_utils.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_token_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otolmach <otolmach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 19:02:11 by otolmach          #+#    #+#             */
+/*   Updated: 2024/04/02 19:02:40 by otolmach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_strncpy(char *dest, char *s, int n)
+{
+	int	i;
+
+	i = 0;
+	while ((s[i] != '\0') && (i < n))
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strndup(t_mnshll *mnshll, char *s, int n)
+{
+	char	*cs;
+
+	cs = (char *)malloc(sizeof(char) * (n + 1));
+	if (!cs)
+	{
+		return (NULL);
+	}
+	cs = ft_strncpy(cs, s, n);
+	return (cs);
+}
+void	free_arrays(char **str_tab, int i)
+{
+	int	j;
+
+	j = 0;
+	while (j < i && str_tab[j] != 0)
+	{
+		free(str_tab[j]);
+		j++;
+	}
+	free(str_tab);
+	return (NULL);
+}
