ae1fbdce1179db8e3c457a0bdbf1c1032e9301a0
Olena <lenanavorska@gmail.com>
2024-04-19 17:36:05 +0200

    execute push a lot of work awaits 💔

--- exe_cutie.c
+++ exe_cutie.c
@@ -6,7 +6,7 @@
 /*   By: otolmach <otolmach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:37:59 by otolmach          #+#    #+#             */
-/*   Updated: 2024/04/19 16:00:04 by otolmach         ###   ########.fr       */
+/*   Updated: 2024/04/19 17:23:32 by otolmach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 void	exe_cutie(t_mnshll *minsh, char **array, char **new_cmd)
 {
-	char	**pa	
+	char	**split_pathvar;
+	char	*path;
+	char	**env;
+
+	if (!array || !array[0] || !array[0][0])
+		//error command not found NULL exit process here??
+		//no exit in next check
+	if (isbuilt(array[0]) == 1)
+		//function where we ft_cmpr string and execute built ins //do we exit the procces here??
+		//no exit in next check
+	if (g_global == SIGPIPE)
+		//free exit process
+	if (!array || !array[0] || !array[0][0] || isbuilt(array[0]) == 1)
+		//free exit process
+	split_pathvar = retrive_path_dir(minsh->env, array[0]); //god please tell me env is finee
+	
+	
 }
+
+
