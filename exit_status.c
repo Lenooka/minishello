41d5809feef0a32a5b6a1a3b259da020816f7aae
Olena <lenanavorska@gmail.com>
2024-04-24 17:50:54 +0200

    reset fds function in progress

--- exit_status.c
+++ exit_status.c
@@ -3,18 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   exit_status.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: olena <olena@student.42.fr>                +#+  +:+       +#+        */
+/*   By: otolmach <otolmach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 18:25:46 by otolmach          #+#    #+#             */
-/*   Updated: 2024/04/24 17:42:50 by olena            ###   ########.fr       */
+/*   Updated: 2024/04/24 17:48:52 by otolmach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* Set exit status to 128 + signal number
+/*
+WIFEXITED and WEXITSTATUS used to know the exit status of the child. 
+WIFEXITED(status) : returns true if the child terminated normally.
+WEXITSTATUS(status) : returns the exit status of the child. 
+This macro should be employed only if WIFEXITED returned true.
+If WIFSIGNALED is true of status , this macro returns the signal 
+number of the signal that terminated the child process.  
+Set exit status to 128 + signal number
         else
-Set exit status to 127 if command not found*/
+Set exit status to 127 if command not found
+
+if i exit my life what would be my exit status
+	-1
+*/
 void exit_status(t_mnshll *minsh, pid_t pid, int com_run)
 {
     int status;
