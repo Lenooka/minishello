272280c60e65dbb4111b383d15cc191c9a29fe1e
Lenooka <136221271+Lenooka@users.noreply.github.com>
2024-04-24 19:11:35 +0000

    freeexitstatus

--- frees.c
+++ frees.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   frees.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: olena <olena@student.42.fr>                +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 14:06:39 by otolmach          #+#    #+#             */
-/*   Updated: 2024/04/24 18:21:05 by olena            ###   ########.fr       */
+/*   Updated: 2024/04/24 19:10:54 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,30 @@ void	free_cmd_list(t_lexer *cmdlist)
 	}
 }
 
-void	ft_exit(t_mnshll *m)
+
+void	free_exit_procces(t_mnshll *minsh, char *mess)
 {
-	m->exit = 1;
-	exit(1);
+	int	exit_s;
+
+	exit_s = minsh->exit;
+	if (minsh->input)
+		free(minsh->input);
+	if (minsh->com_array)
+		free_all_arrays(minsh->com_array);
+	if (minsh->envl)
+		free_env(minsh->envl);
+	if (minsh->list_com)
+		free_cmd_list(minsh->list_com);
+	close(stdin);
+	close(stdout);
+	close(stderr);
+	if (minsh->fdin != -1)
+		close(minsh->fdin);
+	if (minsh->fdout != -1)
+		close(minsh->fdout);
+	if (minsh)
+		free(minsh);
+	rl_clear_history();
+	printf("%s\n", mess);
+	exit(exit_s);			
 }
