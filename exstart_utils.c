f9a1dc28af4fbe1d36476efdff4dce0f93f5e36d
Olena <lenanavorska@gmail.com>
2024-04-25 20:17:49 +0200

    find cm pos

--- exstart_utils.c
+++ exstart_utils.c
@@ -6,13 +6,20 @@
 /*   By: olena <olena@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 18:18:04 by olena             #+#    #+#             */
-/*   Updated: 2024/04/24 18:18:32 by olena            ###   ########.fr       */
+/*   Updated: 2024/04/25 20:17:25 by olena            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	find_com_pos(char **com_array, int	pos)
+int	find_com_pos(char **arr, intn pos)
 {
-	//find position untill "|"
+	int	i;
+
+	i = pos;
+	while (arr[i] && arr[i][0] && ft_strcmp(arr[i], "|" )!= 0)
+		i++;
+	if (arr[i] && arr[i][0] && ft_strcmp(arr[i], "|" )== 0)
+		i++;
+	return (i);
 }
